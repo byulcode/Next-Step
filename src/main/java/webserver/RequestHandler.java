9e80624386955b61d709904cc6964ba67e7b58bd
byulcode <bybit0408@gmail.com>
2023-01-01 13:44:39 +0900

    FEAT : 요구사항1 - index.html 응답하기. Hint만 보고 해결

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +22,25 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            //첫 번째 요구사항 - index.html 응답하기
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+            log.debug("request : {}", line);
+
+            String[] tokens = line.split(" ");
+            String url = tokens[1];
+
+            while (!"".equals(line)) {
+                line = br.readLine();
+                log.debug("line : {}", line);
+            }
+            if (line == null) {
+                return;
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
