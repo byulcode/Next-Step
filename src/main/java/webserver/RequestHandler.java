fa1a530c84cae67003805ecda49a16cfb0ebdf7f
byulcode <bybit0408@gmail.com>
2023-01-02 21:06:36 +0900

    FEAT : 요구사항2 - GET 방식으로 회원가입하기. Hint만 보고 해결

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,10 +2,14 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.URLEncoder;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,26 +25,41 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-            //첫 번째 요구사항 - index.html 응답하기
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            //요구사항1 - index.html 응답하기
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
             log.debug("request : {}", line);
 
             String[] tokens = line.split(" ");
             String url = tokens[1];
 
+            //요구사항2 - GET 방식으로 회원가입하기
+            if (url.startsWith("/user/create?")) {
+                int index = url.indexOf("?");
+                String requestPath = url.substring(0, index);
+                String queryString = url.substring(index + 1);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"),
+                        params.get("name"), params.get("email"));
+                log.debug("UserId : {}", user.getUserId());
+                log.debug("password : {}", user.getPassword());
+                log.debug("name : {}", user.getName());
+                log.debug("email : {}", user.getEmail());
+            }
+
+            if (line == null) {
+                return;
+            }
             while (!"".equals(line)) {
                 line = br.readLine();
                 log.debug("line : {}", line);
             }
-            if (line == null) {
-                return;
-            }
+
+            String path = URLEncoder.encode("./webapp" + url, "UTF-8");
 
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            byte[] body = Files.readAllBytes(new File(path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
