d60ef3ea66e53ced7fdbbf2964fa02cba5032927
byulcode <bybit0408@gmail.com>
2023-01-14 20:09:03 +0900

    TEST : HttpRequest 클래스의 POST 방식 테스트 코드

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -8,15 +8,26 @@ import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class HttpRequestTest {
+public class HttpRequestTest {
     private String testDirectory = "./src/test/resources/";
 
     @Test
-    void request_GET() throws Exception{
+    void request_GET() throws Exception {
         InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
         HttpRequest request = new HttpRequest(in);
 
         assertThat(request.getMethod()).isEqualTo("GET");
+        assertThat(request.getPath()).isEqualTo("/user/create");
+        assertThat(request.getHeater("Connection")).isEqualTo("keep-alive");
+        assertThat(request.getParameter("userId")).isEqualTo("byulcode");
+    }
+
+    @Test
+    void request_POST() throws Exception {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_POST.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertThat(request.getMethod()).isEqualTo("POST");
         assertThat(request.getPath()).isEqualTo("/user/create");
         assertThat(request.getHeater("Connection")).isEqualTo("keep-alive");
         assertThat(request.getParameter("userId")).isEqualTo("byulcode");
