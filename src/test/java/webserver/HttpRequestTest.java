19b068ea26574300f5bb99f4b53f7597a61c5285
byulcode <bybit0408@gmail.com>
2023-01-15 09:31:16 +0900

    TEST : InputStream 생성 메소드 구현

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,7 +14,7 @@ public class HttpRequestTest {
 
     @Test
     public void request_GET() throws Exception {
-        InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        InputStream in = createInputStream("Http_GET.txt");
         HttpRequest request = new HttpRequest(in);
 
         assertThat(request.getMethod()).isEqualTo("GET");
@@ -24,12 +25,16 @@ public class HttpRequestTest {
 
     @Test
     public void request_POST() throws Exception {
-        InputStream in = new FileInputStream(new File(testDirectory + "Http_POST.txt"));
+        InputStream in = createInputStream("Http_POST.txt");
         HttpRequest request = new HttpRequest(in);
 
         assertThat(request.getMethod()).isEqualTo("POST");
         assertThat(request.getPath()).isEqualTo("/user/create");
         assertThat(request.getHeader("Connection")).isEqualTo("keep-alive");
         assertThat(request.getParameter("userId")).isEqualTo("byulcode");
+    }
+
+    private InputStream createInputStream(String filename) throws FileNotFoundException {
+        return new FileInputStream(new File(testDirectory + filename));
     }
 }
