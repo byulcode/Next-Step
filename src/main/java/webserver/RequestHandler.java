2c4d92cc571a7aaadc638258c0aefa3d5e32fd99
byulcode <bybit0408@gmail.com>
2023-01-16 17:57:30 +0900

    REFACTOR : controller와 mapper을 이용해 리팩토링

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,19 +1,16 @@
 package webserver;
 
-import db.DataBase;
+import controller.Controller;
 import http.HttpRequest;
 import http.HttpResponse;
-import model.User;
+import mapper.RequestMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Collection;
-import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,79 +29,17 @@ public class RequestHandler extends Thread {
 
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
-            String path = getDefaultPath(request.getPath());
+            Controller controller = RequestMapping.getController(request.getPath());
 
-            // 회원가입
-            if ("/user/create".equals(path)) {
-                // 읽어온 회원 정보를 담은 user 객체 생성
-                createUser(request, response);
-            }  // 로그인
-            else if ("/user/login".equals(path)) {
-                login(request, response);
-            } //사용자 목록 출력
-            else if ("/user/list".equals(path)) {
-                listUser(request, response);
-            } else {
+            if (controller == null) {
+                String path = getDefaultPath(request.getPath());
                 response.forward(path);
+            } else {
+                controller.service(request, response);
             }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-
-    private void createUser(HttpRequest request, HttpResponse response) {
-        User user = new User(
-                request.getParameter("userId"),
-                request.getParameter("password"),
-                request.getParameter("name"),
-                request.getParameter("email")
-        );
-        log.debug("User : {}", user);
-
-        DataBase.addUser(user);
-        log.debug("Database : {}", DataBase.findUserById(user.getUserId()));
-
-        response.sendRedirect("/index.html");
-    }
-
-    private void login(HttpRequest request, HttpResponse response) {
-        User user = DataBase.findUserById(request.getParameter("userId"));
-
-        // 해당 아이디가 존재하는 경우
-        if (user != null) {
-            if (user.login(request.getParameter("password"))) {//비밀번호 맞을 경우
-                response.addHeader("Set-Cookie", "logined=true");
-                response.sendRedirect("/index.html");
-            } else {// 비밀번호가 틀릴 경우
-                response.sendRedirect("/user/login_failed.html");
-            }
-        } else {// 아이디가 존재하지 않는 경우
-            response.sendRedirect("/user/login_failed.html");
-        }
-    }
-
-    private void listUser(HttpRequest request, HttpResponse response) {
-        if (!isLogin(request.getHeader("Cookie"))) {//로그인하지 않은 경우
-            response.sendRedirect("/user/login.html");
-            return;
-        }
-
-        //로그인 한 경우
-        StringBuilder sb = new StringBuilder();
-        Collection<User> users = DataBase.findAll();
-        log.debug("database.user : {}", users);
-        sb.append("<table border=\"1\">");
-        String prefix = "<td>";
-        String postfix = "</td>";
-        for (User user : users) {
-            sb.append("<tr>");
-            sb.append(prefix + user.getUserId() + postfix);
-            sb.append(prefix + user.getName() + postfix);
-            sb.append(prefix + user.getEmail() + postfix);
-            sb.append("</tr>");
-        }
-        sb.append("</table>");
-        response.forwardBody(sb.toString());
     }
 
     private String getDefaultPath(String path) {
@@ -114,12 +49,4 @@ public class RequestHandler extends Thread {
         return path;
     }
 
-    private boolean isLogin(String cookieValue) {
-        Map<String, String> cookies = HttpRequestUtils.parseCookies(cookieValue);
-        String value = cookies.get("logined");
-        if (value == null) {
-            return false;
-        }
-        return Boolean.parseBoolean(value);
-    }
 }
