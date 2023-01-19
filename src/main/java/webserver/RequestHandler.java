48008585e1c21da4bd6148862f9d7ec0d999b6a1
byulcode <bybit0408@gmail.com>
2023-01-19 20:23:26 +0900

    FEAT : 고유한 세션 아이디 존재여부 체크

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,11 +6,14 @@ import http.HttpResponse;
 import mapper.RequestMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Map;
+import java.util.UUID;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -31,6 +34,11 @@ public class RequestHandler extends Thread {
             HttpResponse response = new HttpResponse(out);
             Controller controller = RequestMapping.getController(request.getPath());
 
+            //세션 아이디 존재 여부 확인
+            if (getSessionId(request.getHeader("Cookie")) == null) {
+                response.addHeader("Set-Cookie","JSESSIONID=" + UUID.randomUUID());
+            }
+
             if (controller == null) {
                 String path = getDefaultPath(request.getPath());
                 response.forward(path);
@@ -47,6 +55,11 @@ public class RequestHandler extends Thread {
             return "/index.html";
         }
         return path;
+    }
+
+    private String getSessionId(String cookieValue) {
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(cookieValue);
+        return cookies.get("JSESSIONID");
     }
 
 }
