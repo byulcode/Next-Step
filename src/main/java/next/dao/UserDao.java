be1b83c5740cb4d615a000741cc8a79f26d4878c
byulcode <bybit0408@gmail.com>
2023-01-30 00:10:46 +0900

    REFACTOR : SelectJdbcTemplate 생성해 findAll, findByUserId 리팩토링

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import core.jdbc.ConnectionManager;
 import core.jdbc.JdbcTemplate;
+import core.jdbc.SelectJdbcTemplate;
 import next.model.User;
 
 public class UserDao {
@@ -29,7 +30,6 @@ public class UserDao {
         jdbcTemplate.update(sql);
     }
 
-
     //회원 update
     public void update(User user) throws SQLException {
 
@@ -47,69 +47,47 @@ public class UserDao {
         jdbcTemplate.update(sql);
     }
 
-
     //모든 회원 반환
     public List<User> findAll() throws SQLException {
-        List<User> users = new ArrayList<>();
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null; //select 결과를 담을 변수
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM USERS";
-            pstmt = con.prepareStatement(sql);
+        String sql = "SELECT * FROM USERS";
+        SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
+            @Override
+            public void setValues(PreparedStatement pstmt) {
 
-            rs = pstmt.executeQuery();
+            }
 
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-                users.add(user);
+            @Override
+            public Object mapRow(ResultSet rs) throws SQLException {
+                User user = new User
+                        (rs.getString("userId"),
+                                rs.getString("password"),
+                                rs.getString("name"),
+                                rs.getString("email"));
+                return user;
             }
-            return users;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+        };
+        return selectJdbcTemplate.query(sql);
     }
 
     //userId로 회원 찾기
     public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+        SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
+            @Override
+            public void setValues(PreparedStatement pstmt) throws SQLException {
+                pstmt.setString(1, userId);
+            }
 
-            rs = pstmt.executeQuery(); //executeQuery()는 쿼리 실행 결과를 ResultSet 타입으로 반환해줌
-
-            User user = null;
-            if (rs.next()) {//다음 행이 없을 때까지 실행
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
+            @Override
+            public Object mapRow(ResultSet rs) throws SQLException {
+                User user = new User
+                        (rs.getString("userId"),
+                                rs.getString("password"),
+                                rs.getString("name"),
+                                rs.getString("email"));
+                return user;
             }
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+        };
+        return (User) selectJdbcTemplate.queryForObject(sql);
     }
 }
