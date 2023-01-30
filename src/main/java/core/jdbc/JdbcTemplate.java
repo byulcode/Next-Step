d403f371a052b9da94e9f5ab89fd416d00396280
byulcode <bybit0408@gmail.com>
2023-01-30 12:26:10 +0900

    REFACTOR : SQLException을 RuntimeException으로 변환해 throw 하도록 변경

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -10,29 +10,19 @@ import java.util.List;
 public class JdbcTemplate {
 
     public void update(String sql, PreparedStatementSetter pstmtSetter) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
             pstmtSetter.setValues(pstmt);
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 
     public List query(String sql, PreparedStatementSetter pstmtSetter, RowMapper rowMapper) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
         ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
             pstmtSetter.setValues(pstmt);
             rs = pstmt.executeQuery();
 
@@ -41,21 +31,17 @@ public class JdbcTemplate {
                 users.add(rowMapper.mapRow(rs));
             }
             return users;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         } finally {
             if (rs != null) {
                 rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
             }
         }
     }
 
     //단건을 조회하기 위한 메소드
-    public Object queryForObject(String sql,PreparedStatementSetter pstmtSetter, RowMapper rowMapper) throws SQLException {
+    public Object queryForObject(String sql, PreparedStatementSetter pstmtSetter, RowMapper rowMapper) throws SQLException {
         List result = new ArrayList();
         result = query(sql, pstmtSetter, rowMapper);
         return result.get(0);
