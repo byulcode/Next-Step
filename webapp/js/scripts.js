d3ce222ae17d16ff72b3a122dac28afab4c3f65a
byulcode <bybit0408@gmail.com>
2023-02-04 12:42:03 +0900

    CHORE : jquery 설정 변경

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,3 +1,4 @@
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
@@ -7,4 +8,32 @@ String.prototype.format = function() {
         ;
   });
 };
+
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+  e.preventDefault(); //submit이 자동으로 동작하는 것을 막는다
+
+  //form 데이터들을 자동으로 묶어준다.
+  var queryString = $("form[name=answer]").serialize();
+
+  $.ajax({
+    type : 'post',
+    url : '/api/qna/addAnswer',
+    data : queryString,
+    dataType : 'json',
+    error : onError,
+    success: onSuccess,
+  });
+}
+
+function onSuccess(json, status){
+  var answerTemplate = $("#answerTemplate").html();
+  var template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId, json.answerId);
+  $(".qna-comment-slipp-articles").prepend(template);
+}
+
+function onError(xhr, status) {
+  alert("error");
+}
 
--- webapp/qna/show.jsp
+++ webapp/qna/show.jsp
@@ -85,19 +85,7 @@
 								</div>
 							</article>
                             </c:forEach>
-							<div class="answerWrite">
-								<form name="answer" method="post">
-									<input type="hidden" name="questionId" value="${question.questionId}">
-									<div class="form-group col-lg-4" style="padding-top:10px;">
-										<input class="form-control" id="writer" name="writer" placeholder="이름">
-									</div>
-									<div class="form-group col-lg-12">
-										<textarea name="contents" id="contents" class="form-control" placeholder=""></textarea>
-									</div>
-									<input class="btn btn-success pull-right" type="submit" value="답변하기" />
-									<div class="clearfix" />
-								</form>
-							</div>
+
 						</div>
 					</div>
 				</div>
