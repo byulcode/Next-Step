4bbf6de5b792dbff314c588b512d9799046ab0fb
byulcode <bybit0408@gmail.com>
2023-02-05 13:18:34 +0900

    FEAT : 답변 삭제 기능 구현

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,4 +1,3 @@
-
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
@@ -9,10 +8,12 @@ String.prototype.format = function() {
   });
 };
 
+
+
 $(".answerWrite input[type=submit]").click(addAnswer);
 
 function addAnswer(e) {
-  e.preventDefault(); //submit이 자동으로 동작하는 것을 막는다
+  e.preventDefault();//submit이 자동으로 동작하는 것을 막는다
 
   //form 데이터들을 자동으로 묶어준다.
   var queryString = $("form[name=answer]").serialize();
@@ -22,8 +23,8 @@ function addAnswer(e) {
     url : '/api/qna/addAnswer',
     data : queryString,
     dataType : 'json',
-    error : onError,
-    success: onSuccess,
+    error: onError,
+    success : onSuccess,
   });
 }
 
@@ -33,7 +34,31 @@ function onSuccess(json, status){
   $(".qna-comment-slipp-articles").prepend(template);
 }
 
-function onError(xhr, status) {
+function onError(request, status, error) {
   alert("error");
+}
+
+$(".qna-comment").on("click", ".form-delete", deleteAnswer);
+
+function deleteAnswer(e) {
+  e.preventDefault();
+
+  var deleteBtn = $(this);
+  var queryString = deleteBtn.closest("form").serialize();
+
+  $.ajax({
+    type: 'post',
+    url: "/api/qna/deleteAnswer",
+    data: queryString,
+    dataType: 'json',
+    error: function (xhr, status) {
+      alert("error");
+    },
+    success: function (json, status) {
+      if (json.status) {
+        deleteBtn.closest('article').remove();
+      }
+    }
+  });
 }
 
