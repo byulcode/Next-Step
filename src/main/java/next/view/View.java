6cdbbb2b7aa4761ffb706607aa5da9a14112727e
byulcode <bybit0408@gmail.com>
2023-02-05 18:14:51 +0900

    FEAT : View 객체들 추가

---
+++ src/main/java/next/view/View.java
@@ -0,0 +1,8 @@
+package next.view;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface View {
+    void render(HttpServletRequest req, HttpServletResponse resp) throws Exception;
+}
