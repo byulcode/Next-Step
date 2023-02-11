8ab59ba87a01c1d6b698f3c28691664d07af128d
byulcode <bybit0408@gmail.com>
2023-02-11 22:58:00 +0900

    문제 13. QnaService 클래스 추가해 컨트롤러의 중복 제거

--- src/main/java/next/controller/qna/ApiDeleteQuestionController.java
+++ src/main/java/next/controller/qna/ApiDeleteQuestionController.java
@@ -3,19 +3,15 @@ package next.controller.qna;
 import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
 import next.controller.UserSessionUtils;
-import next.dao.AnswerDao;
-import next.dao.QuestionDao;
-import next.model.Answer;
-import next.model.Question;
+import next.exception.CannotDeleteException;
 import next.model.Result;
+import next.service.QnaService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class ApiDeleteQuestionController extends AbstractController {
-    private QuestionDao questionDao = QuestionDao.getInstance();
-    private AnswerDao answerDao = AnswerDao.getInstance();
+    private QnaService qnaService = QnaService.getInstance();
 
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -24,38 +20,11 @@ public class ApiDeleteQuestionController extends AbstractController {
         }
 
         long questionId = Long.parseLong(request.getParameter("questionId"));
-        Question question = questionDao.findById(questionId);
-
-        //질문이 없는 경우
-        if (question == null) {
-            return jsonView().addObject("result", Result.fail("존재하지 않는 질문입니다."));
-        }
-
-        //질문자와 로그인된 사용자가 다른 경우
-        if (!question.isSameUser(UserSessionUtils.getUserFromSession(request.getSession()))) {
-            return jsonView().addObject("result", Result.fail("다른 사용자의 글을 삭제할 수 없습니다."));
-        }
-
-        List<Answer> answers = answerDao.findAllByQuestionId(questionId);
-        if (answers.isEmpty()) {
-            questionDao.delete(questionId);
+        try {
+            qnaService.deleteQuestion(questionId, UserSessionUtils.getUserFromSession(request.getSession()));
             return jsonView().addObject("result", Result.ok());
-        }
-
-        boolean canDelete = true;
-        for (Answer answer : answers) {
-            String writer = question.getWriter();
-            if (!writer.equals(answer.getWriter())) {
-                canDelete = false;
-                break;
-            }
-        }
-
-        if (canDelete) {
-            questionDao.delete(questionId);
-            return jsonView().addObject("result", Result.ok());
-        } else {
-            return jsonView().addObject("result", Result.fail("다른 사용자가 추가한 댓글이 존재해 삭제할 수 없습니다."));
+        } catch (CannotDeleteException e) {
+            return jsonView().addObject("result", Result.fail(e.getMessage()));
         }
     }
 }
