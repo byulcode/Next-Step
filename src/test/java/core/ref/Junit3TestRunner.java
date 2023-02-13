a9a7bef9293d81822159f547a40a37088775b998
byulcode <bybit0408@gmail.com>
2023-02-13 11:40:03 +0900

    "test"로 시작하는 메소드 실행하기

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -2,10 +2,21 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Junit3TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Constructor<Junit3Test> constructor = clazz.getConstructor();
+        Junit3Test node = constructor.newInstance();
 
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(node);
+            }
+        }
     }
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 import next.model.User;
 
+import java.util.Arrays;
+import java.util.logging.StreamHandler;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -14,14 +17,18 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        Arrays.stream(clazz.getDeclaredFields()).forEach(f -> logger.debug("constructor : {}", f));
+        Arrays.stream(clazz.getDeclaredConstructors()).forEach(c -> logger.debug("field : {}", c));
+        Arrays.stream(clazz.getDeclaredMethods()).forEach(m -> logger.debug("method : {}", m));
     }
-    
+
     @Test
     public void newInstanceWithConstructorArgs() {
         Class<User> clazz = User.class;
         logger.debug(clazz.getName());
     }
-    
+
     @Test
     public void privateFieldAccess() {
         Class<Student> clazz = Student.class;
