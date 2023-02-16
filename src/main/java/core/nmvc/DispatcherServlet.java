e8c7389d59096350d70fbee7dc87c016290a6ffa
byulcode <bybit0408@gmail.com>
2023-02-16 16:15:07 +0900

    HandlerAdapter 인터페이스로 추상화

--- src/main/java/core/nmvc/DispatcherServlet.java
+++ src/main/java/core/nmvc/DispatcherServlet.java
@@ -21,11 +21,11 @@ import org.slf4j.LoggerFactory;
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-
-    private List<HandlerMapping> mappings = Lists.newArrayList();   //초기화 끝난 HandlerMapping
+    private List<HandlerMapping> mappings = Lists.newArrayList();
+    private List<HandlerAdapter> handlerAdapters = Lists.newArrayList();
 
     @Override
-    public void init() throws ServletException {//초기화
+    public void init() throws ServletException {
         LegacyHandlerMapping lhm = new LegacyHandlerMapping();
         lhm.initMapping();
         AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("next.controller");
@@ -33,10 +33,15 @@ public class DispatcherServlet extends HttpServlet {
 
         mappings.add(lhm);
         mappings.add(ahm);
+        handlerAdapters.add(new ControllerHandlerAdapter());
+        handlerAdapters.add(new HandlerExecutionHandlerAdapter());
     }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String requestUri = req.getRequestURI();
+        logger.debug("Method : {}, Request URI : {}", req.getMethod(), requestUri);
+
         Object handler = getHandler(req);
         if (handler == null) {
             throw new IllegalArgumentException("존재하지 않는 URL입니다.");
@@ -52,9 +57,9 @@ public class DispatcherServlet extends HttpServlet {
         }
     }
 
-    private Object getHandler(HttpServletRequest request) {
+    private Object getHandler(HttpServletRequest req) {
         for (HandlerMapping handlerMapping : mappings) {
-            Object handler = handlerMapping.getHandler(request);
+            Object handler = handlerMapping.getHandler(req);
             if (handler != null) {
                 return handler;
             }
@@ -62,12 +67,14 @@ public class DispatcherServlet extends HttpServlet {
         return null;
     }
 
+
     //요청 URL에 해당하는 컨트롤러를 찾아 메소드를 실행한다.
     private ModelAndView execute(Object handler, HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        if (handler instanceof Controller) {
-            return ((Controller) handler).execute(req, resp);
-        } else {
-            return ((HandlerExecution) handler).handle(req, resp);
+        for (HandlerAdapter handlerAdapter : handlerAdapters) {
+            if (handlerAdapter.supports(handler)) {
+                return handlerAdapter.handle(req, resp, handler);
+            }
         }
+        return null;
     }
 }
